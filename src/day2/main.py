fd8c9f556e4c0d57b7df6da891729f1e7cefb747
nic <nicpayne713@gmail.com>
2020-12-02 08:09:07 -0600

    Add solution #2 for day 2

--- src/day2/main.py
+++ src/day2/main.py
@@ -1,5 +1,6 @@
 from multiprocessing import Pool
 import time
+from more_itertools import locate
 
 
 def get_data(filepath: str = './data/raw/dat2_input.txt'):
@@ -26,6 +27,27 @@ def extract_vars(_input: str) -> tuple:
     return tuple(x)
 
 
+def extract_vars_pt2(_input: str) -> tuple:
+    """
+    extracts x0, x1, l, password from the input and returns them as distinct
+    values in a tuple having mutated x0 and x1 to be 0 indexed
+
+    Note the function is the **almost** the same as extract_vars but I wanted
+    the docstring to have the change in definitions for part 2
+
+    :param _input: of the form "x0-x1 l: <password>" where l is the required
+        letter for the password to be valid, x0 and x1 are required positions
+         that l much be in the password (exactly one of the 2 positions needs to
+         be satisfied) and <password> is obviously the password to check
+    :return: x0, x1, l, password
+    """
+    x = list(' '.join(' '.join(_input.split('-')).split(':')
+                      ).replace('  ', ' ').split(' '))
+    x[0] = int(x[0])-1
+    x[1] = int(x[1])-1
+    return tuple(x)
+
+
 def check_validity(_input) -> bool:
     """
     checks password given a password and it's letter + count requirement
@@ -39,6 +61,19 @@ def check_validity(_input) -> bool:
 
     cnt = p.count(l)
     return f <= cnt <= c
+
+
+def check_validity2(_input) -> bool:
+    """
+    checks password given a password and it's letter + index requirement
+    :param _input:  of the form "x0-x1 l: <password>" where l is the required
+        letter for the password to be valid, x0 and x1 are required positions
+         that l much be in the password (exactly one of the 2 positions needs to
+         be satisfied) and <password> is obviously the password to check
+    """
+    x0, x1, l, p = extract_vars_pt2(_input)
+    indices = list(locate(p, lambda x: x == l))
+    return bool(x0 in indices) ^ bool(x1 in indices)
 
 
 def main(filepath: str = './data/raw/day2_input.txt', mp=False, _map=False):
@@ -62,10 +97,21 @@ def main(filepath: str = './data/raw/day2_input.txt', mp=False, _map=False):
     print(f'Time with MP = {mp} and _map = {_map} was: {end-start}')
 
 
+def main2(filepath: str = './data/raw/day2_input.txt'):
+
+    inputs = get_data(filepath)
+
+    res = map(check_validity2, inputs)
+    print(f'Number of valid passwords for problem 2: {sum(res)}')
+
+
 if __name__ == '__main__':
     # solution 1
-    main('./data/raw/day2_input.txt', mp=True, _map=False)
+    # main('./data/raw/day2_input.txt', mp=True, _map=False)
 
     main('./data/raw/day2_input.txt', mp=False, _map=True)
 
-    main('./data/raw/day2_input.txt', mp=False, _map=False)
+    # main('./data/raw/day2_input.txt', mp=False, _map=False)
+
+    # solution 2
+    main2('./data/raw/day2_input.txt')
