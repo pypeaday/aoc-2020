8a99bb0bec5451d32e85150dfc6805d6f83ec265
Nicholas <nicpayne713@gmail.com>
2020-12-06 11:41:43 -0600

    Add solution #2 for day 6
    Note a refactor of solution 1 stuff from the initial commit

--- src/day6/main.py
+++ src/day6/main.py
@@ -1,30 +1,64 @@
+from functools import reduce
+
+
 def get_data(filepath: str = './data/raw/day6_sample.txt') -> (list, list):
     """
-    Returns list of inputs grouped by gruop
+    Returns raw list of inputs having \n stripped away
     :param filepath:
-    :return: list of inputs, as well as a list containing the number
-        of people per group (inputs are gathered up to the group level)
+    :return: lints of the data file in a list
     """
-    inputs = []
-    num_people_per_group = []
+    data = []
     with open(filepath, 'r') as f:
         lines = f.readlines()
-        _data = ''
-        i = 0
         for line in lines:
-            if line == '\n':
-                inputs.append(_data)
-                num_people_per_group.append(i)
-                _data = ''
-                i = 0
-            else:
-                _data += line.replace('\n', ' ').replace(' ', '')
-                i += 1
-        if _data != '':
-            inputs.append(_data)
-            num_people_per_group.append(i)
+            data.append(line.replace('\n', ''))
 
-    return inputs, num_people_per_group
+    return data
+
+
+def format_data_part_1(raw_data: list) -> list:
+    """
+    Takes raw data file and formats the data to be list
+    of answers by group
+    :param raw_data: direct output of get_data
+    :return:
+    """
+    _data = ''
+    inputs = []
+    for line in raw_data:
+        if line == '':
+            inputs.append(_data)
+            _data = ''
+            _group_answers = []
+        else:
+            _data += line.replace(' ', '')
+    if _data != '':
+        inputs.append(_data)
+
+    return inputs
+
+
+def format_data_part_2(raw_data: list) -> list:
+    """
+    Takes raw data and formats to be list of lists,
+    where the sublists are the individual answers
+    and the lists are separated by group
+
+    :param raw_data: direct output of get_data
+    :return:
+    """
+    inputs = []
+    _group_answers = []
+    for line in raw_data:
+        if line == '':
+            inputs.append(_group_answers)
+            _group_answers = []
+        else:
+            _group_answers.append(line.replace(' ', ''))
+    if _group_answers:
+        inputs.append(_group_answers)
+
+    return inputs
 
 
 def get_group_count(data: str) -> int:
@@ -39,27 +73,47 @@ def get_group_count(data: str) -> int:
     return len(set(data))
 
 
-def sum_all_group_counts(_input: list) -> int:
-    """
-    Sum the group counts of 'yes' answers for a given
-    dataset.
-
-    :param _input: list of results of get_group_count
-    :return:
-    """
-    return sum(_input)
-
-
 def main(filepath: str = './data/raw/day6_sample.txt') -> int:
     """
-    Mainf function for solution 1 day 6. Takes input and calculates
+    Main function for solution 1 day 6. Takes input and calculates
     the sum of 'yes' answers per group then sums each of those counts
 
     :param filepath:
     :return:
     """
-    inputs, _ = get_data(filepath)
-    all_group_counts = map(get_group_count, inputs)
+    raw_data = get_data(filepath)
+    data = format_data_part_1(raw_data)
+    all_group_counts = map(get_group_count, data)
+    res = sum(all_group_counts)
+    return res
+
+
+def _intersection(s1: str, s2: str) -> str:
+    return ''.join([x for x in set(s1).intersection(s2)])
+
+
+def get_intersection(_group: list) -> str:
+    """
+    Take list of group answers and run reduce job to get shared answers
+    by all parties of the group
+    :param _group: list of answers
+        ex: ['a', 'ab', 'ac']
+    :return: for the example would return 'a'
+    """
+    return reduce(_intersection, _group)
+
+
+def main2(filepath: str = './data/raw/dat6_sample.txt') -> int:
+    """
+    Main function for solution 2 day 6. Takes input and calculates
+    the sum of 'yes' answers per group then sums each of those counts
+    :param filepath:
+    :return:
+    """
+    raw_data = get_data(filepath)
+    data = format_data_part_2(raw_data)
+    group_answers = map(get_intersection, data)
+    all_group_counts = map(get_group_count, group_answers)
     res = sum(all_group_counts)
     return res
 
@@ -67,5 +121,9 @@ def main(filepath: str = './data/raw/day6_sample.txt') -> int:
 if __name__ == '__main__':
     # part 1
     res = main('./data/raw/day6_input.txt')
+    print(f'The sum of the count per group of any "yes" answers is: {res}')
+
+    # part 2
+    res = main2('./data/raw/day6_input.txt')
     print(f'The sum of the count per group of any "yes" answers is: {res}')
 
--- test/test_day6.py
+++ test/test_day6.py
@@ -1,40 +1,86 @@
 import pytest
 import os
-from src.day6.main import (get_data, get_group_count, sum_all_group_counts)
+from src.day6.main import (get_data, format_data_part_1, format_data_part_2, get_group_count, get_intersection,
+                           main, main2)
 
 
 os.chdir('..')  # we're in test/and need to be up one level
 
 
 def test_get_data():
-    inputs, n_people_per_group = get_data('./data/raw/day6_sample.txt')
+    raw_data = get_data('./data/raw/day6_sample.txt')
 
-    assert inputs[0] == 'abc'
-    assert inputs[1] == 'abc'
-    assert inputs[2] == 'abac'
-    assert inputs[3] == 'aaaa'
-    assert inputs[4] == 'b'
+    assert raw_data[0] == 'abc'
+    assert raw_data[1] == ''
+    assert raw_data[2] == 'a'
+    assert raw_data[3] == 'b'
+    assert raw_data[4] == 'c'
+    assert raw_data[5] == ''
+    assert raw_data[6] == 'ab'
+    assert raw_data[7] == 'ac'
+    assert raw_data[8] == ''
+    assert raw_data[9] == 'a'
+    assert raw_data[10] == 'a'
+    assert raw_data[11] == 'a'
+    assert raw_data[12] == 'a'
+    assert raw_data[13] == ''
+    assert raw_data[14] == 'b'
 
-    assert n_people_per_group[0] == 1
-    assert n_people_per_group[1] == 3
-    assert n_people_per_group[2] == 2
-    assert n_people_per_group[3] == 4
-    assert n_people_per_group[4] == 1
+
+def test_format_data_part_1():
+    raw_data = get_data('./data/raw/day6_sample.txt')
+    data = format_data_part_1(raw_data)
+    assert data[0] == 'abc'
+    assert data[1] == 'abc'
+    assert data[2] == 'abac'
+    assert data[3] == 'aaaa'
+    assert data[4] == 'b'
+
+
+def test_format_data_part_2():
+    raw_data = get_data('./data/raw/day6_sample.txt')
+    data = format_data_part_2(raw_data)
+
+    assert data[0][0] == 'abc'
+
+    assert data[1][0] == 'a'
+    assert data[1][1] == 'b'
+    assert data[1][2] == 'c'
+
+    assert data[2][0] == 'ab'
+    assert data[2][1] == 'ac'
+
+    assert data[3][0] == 'a'
+    assert data[3][1] == 'a'
+    assert data[3][2] == 'a'
+    assert data[3][3] == 'a'
+
+    assert data[4][0] == 'b'
 
 
 def test_get_group_count():
-    inputs, n_people_per_group = get_data('./data/raw/day6_sample.txt')
-    assert get_group_count(inputs[0]) == 3
-    assert get_group_count(inputs[1]) == 3
-    assert get_group_count(inputs[2]) == 3
-    assert get_group_count(inputs[3]) == 1
-    assert get_group_count(inputs[4]) == 1
+    raw_data = get_data('./data/raw/day6_sample.txt')
+    data = format_data_part_1(raw_data)
+    assert get_group_count(data[0]) == 3
+    assert get_group_count(data[1]) == 3
+    assert get_group_count(data[2]) == 3
+    assert get_group_count(data[3]) == 1
+    assert get_group_count(data[4]) == 1
 
 
-def test_sum_all_group_counts():
-    inputs, n_people_per_group = get_data('./data/raw/day6_sample.txt')
-    all_group_counts = []
-    for _data in inputs:
-        all_group_counts.append(get_group_count(_data))
+def test_part_1():
+    assert main('./data/raw/day6_sample.txt') == 11
 
-    assert sum_all_group_counts(all_group_counts) == 11
+
+def test_get_intersection():
+    raw_data = get_data('./data/raw/day6_sample.txt')
+    data = format_data_part_2(raw_data)
+    assert get_intersection(data[0]) == 'abc'
+    assert get_intersection(data[1]) == ''
+    assert get_intersection(data[2]) == 'a'
+    assert get_intersection(data[3]) == 'a'
+    assert get_intersection(data[4]) == 'b'
+
+
+def test_part_2():
+    assert main2('./data/raw/day6_sample.txt') == 6
