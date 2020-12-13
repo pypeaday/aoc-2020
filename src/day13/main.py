8220b4fb85fefe0bb2e1ff2f3e9a0f0ef98d46d8
Nicholas <nicpayne713@gmail.com>
2020-12-13 14:40:04 -0600

    Add solution 2 for day 13

--- src/day13/main.py
+++ src/day13/main.py
@@ -9,29 +9,118 @@ def get_data(filepath: str = "./data/raw/day13_sample.txt"):
         data.append(int(lines[0]))
         for line in lines[1].split(","):
             if line == "x":
-                continue
-            data.append(int(line))
+                data.append(line)
+            else:
+                data.append(int(line))
 
     return data
 
 
+def get_part_2_inputs(data: list):
+    """Returns 2 lists formatted for solving the
+    Chinese Remainder Theorem solving for t such that:
+    t = a[0] mod n[0]
+    t = a[1] mod n[1]
+    ...
+
+    Args:
+        data (list): raw data from get_data()
+
+    Returns:
+        n, a: principle inputs, remainders
+    """
+    a = []
+    n = []
+    for i, x in enumerate(data[1:]):
+        if x == "x":
+            continue
+        a.append((x - i) % x)
+        n.append(x)
+
+    return n, a
+
+
 def calculate_solution_1(filepath: str = "./data/raw/day13_sample.txt"):
     data = get_data(filepath)
+    bus_ids = [x for x in data[1:] if x != "x"]
     next_timestamps = map(
         get_next_departure_time,
-        [data[0] for _ in range(len(data[1:]))],
-        data[1:],
+        [data[0] for _ in range(len(bus_ids))],
+        bus_ids,
     )
     ts = [x for x in next_timestamps]
     next_ts = min(ts)
     bus_id_index = ts.index(next_ts)
-    bus_id = data[1:][bus_id_index]
+    bus_id = bus_ids[bus_id_index]
     delta = next_ts - data[0]
     return delta * bus_id
 
 
-def calculate_solution_2(filepath: str = "./dataraw/day13_sample.txt"):
-    pass
+def extended_euclidean(a: int, b: int):
+    """extension to the Euclidean algorithm, and
+     computes, in addition to the greatest common divisor
+     (gcd) of integers a and b, also the coefficients of
+     Bézout's identity
+     # https://en.wikipedia.org/wiki/Extended_Euclidean_algorithm
+
+     ax + by = gcd(a, b)
+
+    Args:
+        a (int):
+        b (int):
+
+    Returns:
+        [type]: greatest common divisor, bezout's id coefficients
+    """
+    if a == 0:
+        return (b, 0, 1)
+    else:
+        g, y, x = extended_euclidean(b % a, a)
+        return g, x - (b // a) * y, y
+
+
+def inverse_modulo(a, m):
+    """Return inverse modulo
+
+    Args:
+        a ([type]): remainder
+        m ([type]): modulo
+
+    Returns:
+        [type]: modulo inverse
+    """
+    g, x, y = extended_euclidean(a, m)
+    return x % m
+
+
+def crt(m: list, a: list):
+    """Chinese Remainder Algorithm over multiple inputs
+
+    Args:
+        m (list): list of modulii
+        a (list): list of remainders
+
+    Returns:
+        [type]: t such that t = a[i] mod(m[i])
+    """
+    while True:
+        temp1 = (
+            inverse_modulo(m[1], m[0]) * a[0] * m[1]
+            + inverse_modulo(m[0], m[1]) * a[1] * m[0]
+        )
+        temp2 = m[0] * m[1]
+        if len(a) > 2:
+            a = [temp1 % temp2] + a[2:]
+        else:
+            return [temp1 % temp2]
+        m = [temp2] + m[2:]
+
+
+def calculate_solution_2(filepath: str = "./data/raw/day13_sample.txt"):
+    data = get_data(filepath)
+    n, a = get_part_2_inputs(data)
+    res = crt(n, a)
+    return res
 
 
 def main(filepath: str = "./data/raw/day13_sample.txt."):
--- test/test_day13.py
+++ test/test_day13.py
@@ -1,7 +1,10 @@
 from src.day13.main import (
     get_next_departure_time,
     calculate_solution_1,
+    calculate_solution_2,
     get_data,
+    get_part_2_inputs,
+    crt,
 )
 
 
@@ -20,3 +23,41 @@ def test_get_next_departure_time():
 def test_calculate_solution_1():
     res = calculate_solution_1("./data/raw/day13_sample.txt")
     assert res == 295
+
+
+def test_get_part_2_inputs():
+    data = get_data()
+    n, a = get_part_2_inputs(data)
+    assert n == [7, 13, 59, 31, 19]
+    assert a == [0, 12, 55, 25, 12]
+
+
+def test_crt():
+    data = get_data()
+    n, a = get_part_2_inputs(data)
+    res = crt(n, a)
+    assert res == 1068781
+
+    data = [939, 17, "x", 13, 19]
+    n, a = get_part_2_inputs(data)
+    assert crt(n, a) == 3417
+
+    data = [939, 67, 7, 59, 61]
+    n, a = get_part_2_inputs(data)
+    assert crt(n, a) == 754018
+
+    data = [939, 67, "x", 7, 59, 61]
+    n, a = get_part_2_inputs(data)
+    assert crt(n, a) == 779210
+
+    data = [939, 67, 7, "x", 59, 61]
+    n, a = get_part_2_inputs(data)
+    assert crt(n, a) == 1261476
+
+    data = [939, 1789, 37, 47, 1889]
+    n, a = get_part_2_inputs(data)
+    assert crt(n, a) == 1202161486
+
+
+def test_calculate_solution_2():
+    assert calculate_solution_2("./data/raw/day13_sample.txt") == 1068781
